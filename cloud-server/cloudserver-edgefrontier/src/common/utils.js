3a79d081ee0f7ebca5376e682085bc8155857611
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-11-18 16:45:29 +0700

    update

--- cloud-server/cloudserver-edgefrontier/src/common/utils.js
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react';
-
-export const lazyComponent = (name, importer) =>
-  lazy(async () => {
-    const component = await importer;
-    return { default: component[name] };
-  });
